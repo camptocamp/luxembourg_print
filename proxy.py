9a47be8f277bcda5d02ad5ed8fb73ac1918e33e8
Frederic Junod <frederic.junod@camptocamp.com>
2013-10-28 11:45:19 +0100

    Forward the HTTP referer

--- proxy.py
+++ proxy.py
@@ -5,6 +5,7 @@ def get_pdf(request, url="http://map.geoportail.lu/print"):
     # FIXME: forward headers ?
     # FIXME: forward GET params ?
     session = requests.Session()
+    session.headers['Referer'] = request.referer 
     r = session.post(url + "/create.json?url=", data=request.body)
     # FIXME: errors checking
     pdf = r.json().get('getURL')
