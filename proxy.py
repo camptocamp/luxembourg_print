23aaed0e3d1943f580b0b39eb54f9d2f4bc0b380
Frederic Junod <frederic.junod@camptocamp.com>
2013-10-28 10:26:54 +0100

    Add print proxy skeleton

---
+++ proxy.py
@@ -0,0 +1,13 @@
+from cStringIO import StringIO
+import requests
+
+def get_pdf(request, url="http://map.geoportail.lu/print"):
+    # FIXME: forward headers ?
+    # FIXME: forward GET params ?
+    session = requests.Session()
+    r = session.post(url + "/create.json?url=", data=request.body)
+    # FIXME: errors checking
+    pdf = r.json().get('getURL')
+    r = session.get(url + pdf)
+    # FIXME: errors checking
+    return StringIO(r.content)
